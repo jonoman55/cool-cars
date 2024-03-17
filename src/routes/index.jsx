768df808c1a5c71247b18a3d08967f45474fb9c4
jonoman55 <jonoman55@yahoo.com>
2024-03-17 15:55:05 -0400

    update routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,32 +1,35 @@
+// TODO: Update BrowserRouter to HashRouter before Netlify deployment
+
 import {
   BrowserRouter as Router,
   Routes as Switch,
   Route,
+  Navigate,
 } from 'react-router-dom';
 
+import { Layout } from '../components';
 import HomePage from '../pages/HomePage';
 import DesignPage from '../pages/DesignPage';
 import SpecsPage from '../pages/SpecsPage';
 import ProtoTypePage from '../pages/ProtoTypePage';
 import LookAndFeelPage from '../pages/LookAndFeelPage';
 import NotFoundPage from '../pages/NotFoundPage';
-import { Layout } from '../components';
 
 /**
- * App Routes
+ * Routes
  */
 const Routes = () => (
   <Router>
     <Layout>
       <Switch>
-        <Route path="/">
-          <Route index element={<HomePage />} />
-          <Route path="design" element={<DesignPage />} />
-          <Route path="specs" element={<SpecsPage />} />
-          <Route path="proto" element={<ProtoTypePage />} />
-          <Route path="lookfeel" element={<LookAndFeelPage />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/design" element={<DesignPage />} />
+        <Route path="/specs" element={<SpecsPage />} />
+        <Route path="/proto" element={<ProtoTypePage />} />
+        <Route path="/lookfeel" element={<LookAndFeelPage />} />
+        <Route path="/404" element={<NotFoundPage />} />
+        <Route path="*" element={<Navigate to="/404" />} />
       </Switch>
     </Layout>
   </Router>
